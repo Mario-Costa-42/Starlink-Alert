3cbee29b4510e91219e7c9d678d56334a8f8ba9b
Mário Costa <Gearoftech2023@gmail.com>
2025-03-10 20:25:37 -0300

    The code is clean

--- test.py
+++ test.py
@@ -1,37 +1,3 @@
-# from bs4 import BeautifulSoup
-# import requests
-
-# html_text = requests.get('https://findstarlink.com/#3451668;3').text
-# soup = BeautifulSoup(html_text, 'lxml')
-
-
-
-# resultBox = soup.find_all('div', id = 'goodTimingsBlock')
-# print(resultBox)
-
-# resultBoxAvarege = soup.find_all('div', id = 'avgTimingsBlock')
-# print(resultBoxAvarege)
-
-# import sys
-
-# if len(sys.argv) > 1:
-#     arrayWithAllInformation = sys.argv[1]
-#     print(f"the information was received {arrayWithAllInformation}")
-# else:
-#     print("No data received.")
-
-
-
-
-
-
-
-
-
-
-
-
-
 import sys
 import json
 
