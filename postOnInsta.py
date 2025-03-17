ecf939ed88b2776ce1f8dfe11ca7e47a16e34932
Mário Costa <Gearoftech2023@gmail.com>
2025-03-17 12:59:23 -0300

    the login is working

--- postOnInsta.py
+++ postOnInsta.py
@@ -1,2 +1,19 @@
 from instagrapi import Client
 
+client = Client()
+
+USERNAME = "**********"
+PASSWORD = "**********"
+
+client.login(USERNAME, PASSWORD)
+
+try:
+    user_info = client.account_info()
+    print(f"Logged in as: {user_info.username}")
+except Exception:
+    print("Not logged in.")
+
+
+
+#this code is working but I need to edit the image before 
+#posting it on instagram story 
